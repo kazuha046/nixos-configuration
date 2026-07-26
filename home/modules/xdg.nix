cc008768b5552172ff893a360d454efb6d130cc4
Kirill <molin4670@gmail.com>
2026-07-26 20:47:22 +0300

    feat: added wlr

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -28,6 +28,7 @@
         default = [
           "gnome"
           "gtk"
+          "wlr"
         ];
 
         "org.freedesktop.impl.portal.FileChooser" = [ "gnome" ];
@@ -39,6 +40,7 @@
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal-gnome
+      xdg-desktop-portal-wlr
     ];
   };
 
