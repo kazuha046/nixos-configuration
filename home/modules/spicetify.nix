f54de8d5ddbb4b63093068ab8e081b57a4d1fbf1
Kirill <molin4670@gmail.com>
2026-08-01 21:40:43 +0300

    feat: added spotify theme

--- home/modules/spicetify.nix
+++ home/modules/spicetify.nix
@@ -6,6 +6,31 @@ in
 {
   programs.spicetify = {
     enable = true;
+    experimentalFeatures = true;
+    alwaysEnableDevTools = true;
+
+    theme = spicePkgs.themes.sleek;
+
+    colorScheme = "custom";
+
+    customColorScheme = {
+      text = "e7e0e8";
+      subtext = "cbc4cf";
+      main = "49454e";
+      highlight = "cbc4cf";
+      misc = "948f99";
+      notification = "f0b7c5";
+      notification-error = "ffb4ab";
+      shadow = "000000";
+      card = "2b292f";
+      player = "49454e";
+      sidebar = "151218";
+      main-elevated = "36343a";
+      selected-row = "cbc4cf";
+      button = "d2bcfd";
+      button-active = "d2bcfd";
+      button-disabled = "948f99";
+    };
 
     enabledExtensions = with spicePkgs.extensions; [
       adblock
